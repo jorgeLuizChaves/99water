ec3e4fbb34fa13557e00aeaa5f25a6ca51a3c01e
Jorge Oliveira <jorge.luizchaves@yahoo.com.br>
2015-07-11 12:04:34 -0300

    first commit ;)

---
+++ app/src/main/java/labs/kuka/iwater/SlideIntro.java
@@ -0,0 +1,15 @@
+package labs.kuka.iwater;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class SlideIntro extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_slide_intro);
+    }
+}
